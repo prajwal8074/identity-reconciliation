82e55435eff6a11d75675a62b94d1a3721b0d11d
Prajwal <prajwal8074@gmail.com>
2026-03-01 00:14:33 +0530

    deployed port render

--- app.py
+++ app.py
@@ -145,4 +145,5 @@ def identify():
         conn.close()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host='0.0.0.0', port=port)
